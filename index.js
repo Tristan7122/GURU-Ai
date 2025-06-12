44a9582b2824586c157335ba0ef898274cd8483b
Tristan7122 <154232254+Tristan7122@users.noreply.github.com>
2025-06-12 07:31:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ let pairingCode = null
 let isConnected = false
 let botProcess = null
 let botStats = null 
-const mongodbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017'
-const phoneNumber = process.env.PHONE_NUMBER || ''
+const mongodbUri = process.env.MONGODB_URI || 'mongodb+srv://herrison:wells@cluster0.tqhtuou.mongodb.net/?retryWrites=true&w=majority'
+const phoneNumber = process.env.PHONE_NUMBER || '27689344972'
 
 figlet(
   'GURU BOT',
